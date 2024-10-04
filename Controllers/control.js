a04ec401f68f161fbca1916ae9d1249a6d3c84f1
Imasuengarret <garretharry@gmail.com>
2024-10-04 12:33:49 +0100

    mongoose update, delete and get method by id

--- Controllers/control.js
+++ Controllers/control.js
@@ -1,14 +1,41 @@
 const Task = require('../Models/task')
 
-const getAllTasks = (req, res)=>{
-    res.send('All Task')
+const getAllTasks = async (req, res)=>{
+    try {
+        const task = await Task.find({})
+        res.status(200).json({task})
+    } catch (err) {
+        res.status(400).json({error: err.message})
+    }
+    // res.send('All Task')
 }
 
-const getATask = (req, res) =>{
-    res.send('A Task')
+const getATask = async (req, res) =>{
+    try {
+        const _id = req.params.id;
+        const task = await Task.findById(_id)
+        res.status(200).json({task})
+        if (!task){
+            console.log('task not found')
+        }
+    } catch (err) {
+        res.status(400).json({error: err.message})
+    }
+    // res.send('A Task')
 }
-const updateTask = (req, res) =>{
-    res.send('updateTAsk')
+const updateTask =async (req, res) =>{
+    try {
+        const _id = req.params.id
+        const task = await Task.findByIdAndUpdate({_id}, req.body, {
+            new: true,
+            runValidators: true,
+        })
+        res.status(200).json({task})
+    } catch (err) {
+        res.status(400).json({error: err.message})
+    }
+
+    // res.send('updateTAsk')
 }
 const createTask = async (req, res) =>{
     try {
@@ -17,10 +44,17 @@ const createTask = async (req, res) =>{
     } catch (err) {
         res.status(400).json({error: err.message})
     }
+    // res.send('createTask')
 }
 
-const deleteTAsk = (req, res) =>{
-    res.send('delete task')
+const deleteTAsk =  async (req, res) =>{
+    try {
+        const _id = req.params.id
+        const task = await Task.findByIdAndDelete(_id)
+        res.status(200).json('Deleted Succesfully')
+    } catch (err) {
+        res.status(400).json({error: err.message}) 
+    }
 } 
 
 module.exports=  {getAllTasks, getATask, updateTask, createTask, deleteTAsk}
